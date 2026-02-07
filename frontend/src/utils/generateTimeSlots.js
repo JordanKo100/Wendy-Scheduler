b39143620b8067eaa7b1d6ffd1b4da7b83011e40
JordanKo100 <ejordanko@gmail.com>
2026-02-06 20:24:33 -0800

    updated time slots in 30min increments

--- frontend/src/utils/generateTimeSlots.js
+++ frontend/src/utils/generateTimeSlots.js
@@ -1,14 +1,32 @@
-export default function generateTimeSlots(){
+export default function generateTimeSlots() {
     const slots = [];
-    for (let hour = 11; hour < 18; hour++){
-        const startHour = hour % 12 === 0 ? 12 : hour % 12;
-        const startPeriod = hour < 12 ? "AM" : "PM";
-
-        const endHour = (hour + 1) % 12 === 0 ? 12 : (hour + 1) % 12;
-        const endPeriod = (hour + 1) < 12 ? "AM" : "PM";
-
-        slots.push(`${startHour}:00 ${startPeriod} - ${endHour}:00 ${endPeriod}`);
-    }
     
+    // 11 to 17 (which represents the 5:00 PM to 6:00 PM slot)
+    for (let hour = 11; hour < 18; hour++) {
+        // We iterate twice per hour: once for 0 minutes, once for 30 minutes
+        for (let minutes of [0, 30]) {
+            
+            // 1. Calculate Start Time
+            const startHour = hour % 12 === 0 ? 12 : hour % 12;
+            const startPeriod = hour < 12 ? "AM" : "PM";
+            const startMin = minutes === 0 ? "00" : "30";
+
+            // 2. Calculate End Time (30 minutes later)
+            let endHourRaw = hour;
+            let endMinRaw = minutes + 30;
+
+            if (endMinRaw === 60) {
+                endHourRaw++;
+                endMinRaw = 0;
+            }
+
+            const endHour = endHourRaw % 12 === 0 ? 12 : endHourRaw % 12;
+            const endPeriod = endHourRaw < 12 ? "AM" : "PM";
+            const endMin = endMinRaw === 0 ? "00" : "30";
+
+            slots.push(`${startHour}:${startMin} ${startPeriod} - ${endHour}:${endMin} ${endPeriod}`);
+        }
+    }
+
     return slots;
 }
